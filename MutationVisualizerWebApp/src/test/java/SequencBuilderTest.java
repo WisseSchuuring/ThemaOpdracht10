aaf897e80f74234c810f2ed2036536fa08d97dd2
WisseSchuuring <w.s.f.schuuring@st.hanze.nl>
2022-01-19 11:51:14 +0100

    made the SequenceBuilderTest file functional

--- MutationVisualizerWebApp/src/test/java/SequencBuilderTest.java
+++ MutationVisualizerWebApp/src/test/java/SequencBuilderTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SequencBuilderTest {
+import model.SequenceBuilder;
+
+class SequenceBuilderTest {
+    public static void main(String[] args) {
+
+        SequenceBuilder builder = new SequenceBuilder("ATGGTACGCTACGACTCAGCACACATCAGCAGCATG");
+
+        System.out.println(builder.createAminoAcid(builder.dna));
+
+        System.out.println(builder.createComplementStrand(builder.dna));
+
+        System.out.println(builder.createRNAStrand(builder.dna));
+    }
 }
